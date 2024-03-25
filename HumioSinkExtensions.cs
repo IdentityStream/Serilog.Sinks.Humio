c0f3a357bb7fb517f2731b49335ef085258ee82a
Kristian Hellang <kristian@identitystream.com>
2024-03-25 14:13:42 +0100

    Bump to latest dependencies

--- HumioSinkExtensions.cs
+++ HumioSinkExtensions.cs
@@ -1,12 +1,20 @@
 using Serilog.Configuration;
 using Serilog.Events;
+using Serilog.Sinks.PeriodicBatching;
+using System;
 
 namespace Serilog.Sinks.Humio {
 	public static class HumioSinkExtensions {
 		public static LoggerConfiguration Humio(
 				  this LoggerSinkConfiguration loggerConfiguration,
 				  string ingestToken = null, string url = "https://cloud.humio.com", LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum) {
-			return loggerConfiguration.Sink(new Humio(ingestToken, url), restrictedToMinimumLevel);
+			var batchingOptions = new PeriodicBatchingSinkOptions
+			{
+				Period = TimeSpan.FromSeconds(5),
+				BatchSizeLimit = 10,
+			};
+			var sink = new PeriodicBatchingSink(new Humio(ingestToken, url), batchingOptions);
+			return loggerConfiguration.Sink(sink, restrictedToMinimumLevel);
 		}
 	}
 }
